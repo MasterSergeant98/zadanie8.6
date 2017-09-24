4bbf4c4ce5e9b25aa9620bcf7df4cd371ed6f95f
Patryk <master_sergeant98@interia.pl>
2017-09-25 01:05:26 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var a = 8;
+var b = 12;
+var value = (a * a) + (2 * a * b) - (b * b);
+
+console.log(value);
+
+
+if (value>0){
+	console.log('Wynik dodatni');
+}else if (value<0){
+	console.log('Wynik ujemny');
+}
+
+var overZero = Value = 0? 'Wynik jest równy zero' : 'Wynik nie jest równy zero';
+
+console.log(overZero);
